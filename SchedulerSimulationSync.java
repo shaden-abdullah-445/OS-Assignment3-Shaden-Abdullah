783ff294e2a731d2a9ca51103314753de1530b64
shaden-abdullah-445 <445052024@std.psau.edu.sa>
2026-04-30 16:59:13 +0300

    Implement CPU semaphore acquisition and release
    Acquire and release CPU semaphore to manage process execution.

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -147,6 +147,9 @@ class Process implements Runnable {
     public void run() {
         // TODO #3: Acquire CPU semaphore before executing
         // This ensures only allowed number of processes run simultaneously
+        try {
+            SharedResources.cpuSemaphore.acquire();
+
 
         try {
             if (startTime == -1) {
@@ -210,6 +213,13 @@ class Process implements Runnable {
         } finally {
             // TODO #4: Release CPU semaphore here
             // Always release in finally block to prevent deadlocks!
+                            SharedResources.cpuSemaphore.release();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(Colors.RED + "  ✗ " + name + " semaphore interrupted." + Colors.RESET);
+        }
+    }
+    
         }
     }
 
